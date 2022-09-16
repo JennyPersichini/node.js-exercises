a42ee73e1eaf452539dc3f214ef69ae8e5559093
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-09-16 11:25:51 +0200

    Ex02_complete

--- Explore_with_Node.js_REPL_2/Exercise2.js
+++ Explore_with_Node.js_REPL_2/Exercise2.js
@@ -2,8 +2,9 @@ const crypto = require("crypto");
 
 const randomID = crypto.randomUUID();
 
-console.log("Random ID:", randomID);
+console.log(randomID);
 
 
-//In terminale mi torna Random ID: 36fb9445-7ad0-498e-b3fc-cab137dda2a2
+//In terminale mi torna "36fb9445-7ad0-498e-b3fc-cab137dda2a2"
+
 
